c9acc32c7799486a02ead831b35557da3cbab860
Timpan42 <tim@fagerdal.se>
2023-09-12 10:34:12 +0200

    💬 Walter White Ending finns nu

--- program.js
+++ program.js
@@ -26,7 +26,7 @@ export function program(element) {
         CheckEndGame(position)
 
         //ut side the story
-        if (position >= json.length) {
+        if (position >= json.length || position == null) {
             removeContent()
             printErrorOrEndScene("Story dose not exist", "There is no more story, sorry! Want to play again?", "Yes")
 
